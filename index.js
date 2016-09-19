4d0dde5af17f1fb6ebac72cf18ccbb4f5466e0a7
Archis Gore <archis@concurix.com>
2016-09-18 19:12:05 -0700

    Almost there....

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var AWSCognito = require('amazon-cognito-identity-js');
 var MockBrowser = require('mock-browser').mocks.MockBrowser;
+var AWS = require('aws-sdk');
 
 /*
     This function retrieves AWS access credentials for a given
@@ -18,7 +19,8 @@ var MockBrowser = require('mock-browser').mocks.MockBrowser;
             Password: 'password',
             UserPoolId: 'userPoolId',
             ClientId: 'userPoolClientId',
-            IdentityPoolId: 'identityPoolId'
+            IdentityPoolId: 'identityPoolId',
+            AWSRegion: 'us-east-1'
         }
 
     Callback: function(err, creds) {
@@ -42,20 +44,31 @@ exports.getAWSAccessCredentialsForCognitoUser = function(params, callback) {
             Pool : userPool
         };
         var cognitoUser = new AWSCognito.CognitoUser(userData);
+
         cognitoUser.authenticateUser(authenticationDetails, {
             onSuccess: function (result) {
-                console.log('access token + ' + result.getAccessToken().getJwtToken());
-
-                cognitoUserPoolLoginProvider = 'cognito-idp.us-east-1.amazonaws.com/' + params.UserPoolId;
+                //cognitoUserPoolLoginProvider = 'cognito-idp.' + params.AWSRegion +  '.amazonaws.com/' + params.UserPoolId;
+                //var Logins = {};
+                //Logins[cognitoUserPoolLoginProvider] = result.getIdToken().getJwtToken();
+                                    }
                 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
                     IdentityPoolId : params.IdentityPoolId, // your identity pool id here
-                    Logins : {
-                        // Change the key below according to the specific region your user pool is in.
-                        cognitoUserPoolLoginProvider : result.getIdToken().getJwtToken()
-                    }
+                    Logins: Logins
                 });
 
-                console.log(AWS.config.credentials);
+                AWS.config.update({region: params.AWSRegion});
+
+                AWS.config.credentials.get(function(err) {
+                    console.log(err);
+                    console.log(AWS.config.credentials);
+                    var creds = {
+                        AccessKeyId: AWS.config.credentials.accessKeyId,
+                        SecretAccessKey: AWS.config.credentials.secretAccessKey,
+                        SessionToken: AWS.config.credentials.sessionToken,
+                    }
+
+                    callback(null, creds);
+                });
             },
 
             onFailure: function(err) {
--- test/test.js
+++ test/test.js
@@ -1,11 +1,23 @@
 var index = require("../index.js");
 
+/*
+{"id":"01234567-89ab-cdef-0123-456789abcdef",
+"config":{"COGNITO_USERNAME":"Heroku-Addon-User-01234567-89ab-cdef-0123-456789abcdef",
+"COGNITO_PASSWORD":"C4$j1%f1#f4*S5&I1@b7$b1~Q6(C7%f3*G4`d1`C3&l4(j1[b3#",
+"COGNITO_IDENTITY_POOL_ID":"us-east-1:07e91600-b6bc-41e9-a07e-1de3b9d2bdfd",
+"COGNITO_USER_POOL_ID":"arn:aws:cognito-idp:us-east-1:507760724064:userpool/us-east-1_QY61qoJbZ",
+"COGNITO_REGION":"us-east-1",
+"COGNITO_CLIENT_ID":"6a2ae7i6mvd9u6bt9hvpa9c8i2",
+"S3_BUCKET":"scrambled-binaries"},
+*/
+
 var params = {
     Username: 'Heroku-Addon-User-01234567-89ab-cdef-0123-456789abcdef',
-    Password: 'b5&f2$n5$C2&r4`G4@b1&d1#b1]E1)j4+b1)d2@W1&M1(b1|l6*',
+    Password: 'C4$j1%f1#f4*S5&I1@b7$b1~Q6(C7%f3*G4`d1`C3&l4(j1[b3#',
     UserPoolId: 'us-east-1_QY61qoJbZ',
     ClientId: '6a2ae7i6mvd9u6bt9hvpa9c8i2',
-    IdentityPoolId: 'us-east-1:07e91600-b6bc-41e9-a07e-1de3b9d2bdfd'
+    IdentityPoolId: 'us-east-1:07e91600-b6bc-41e9-a07e-1de3b9d2bdfd',
+    AWSRegion: 'us-east-1'
 }
 
 function callback(err, creds) {
